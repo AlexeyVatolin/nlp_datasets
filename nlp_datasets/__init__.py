6f7ebc3d3d7fecc56575838ee04880189db309e2
Alexey Vatolin <vatolinalex@gmail.com>
2020-05-12 21:50:55 +0300

    Edit setup.py for correct import

--- nlp_datasets/__init__.py
+++ nlp_datasets/__init__.py
@@ -1,1 +1,3 @@
-from .data import load_agrr, load_banki_ru, load_lenta, load_reviews
+from nlp_datasets.data import load_agrr, load_banki_ru, load_lenta, load_reviews
+
+# __all__ = ["load_agrr", "load_banki_ru", "load_lenta", "load_reviews"]
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="nlp_datasets",
     version="0.0.1",
-    packages=["nlp_datasets"],
+    packages=find_packages(),  # exclude=('tests.*', 'tests')
     install_requires=["pandas", "requests", "tqdm"],
+    package_data={"nlp_datasets": ["urls/urls.json"]},
     url="",
     license="",
     author="Vatolin Alexey",
