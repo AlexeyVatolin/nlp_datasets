55cd2c4b52955f5cde6fbeaf55a4877a5bf8cc69
Alexey Vatolin <vatolinalex@gmail.com>
2020-05-12 16:56:14 +0300

    Fix __init__ (2)

--- nlp_datasets/__init__.py
+++ nlp_datasets/__init__.py
@@ -1,1 +1,1 @@
-from .data import *
+from .data import load_agrr, load_banki_ru, load_lenta, load_reviews
---
+++ nlp_datasets/data/__init__.py
@@ -0,0 +1,4 @@
+from .reviews import load_reviews
+from .lenta import load_lenta
+from .banki_ru import load_banki_ru
+from .agrr import load_agrr
